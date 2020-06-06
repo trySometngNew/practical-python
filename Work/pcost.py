67cff68ae92f13bb676c9cc7ebb441c0721a7d99
Sourabh Lonikar <sourabhlonikar@gmail.com>
2020-06-06 18:03:50 +0530

    Exercise 1.27: Reading a data file My Solution

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,9 +1,13 @@
-<<<<<<< HEAD
 # pcost.py
-#
+# 
 # Exercise 1.27
-=======
-# pcost.py
-#
-# Exercise 1.27
->>>>>>> 4646e227676c36c539d6839dfd9244ff67683897
+total_shares_cost = 0.0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    header = next(f);
+    for line in f:
+        share_entry_list = line.split(',')
+        share_cost = int(share_entry_list[1]) * float(share_entry_list[2])
+        total_shares_cost = total_shares_cost + share_cost
+
+print(f'Total Portfolio Value is ${total_shares_cost}')
