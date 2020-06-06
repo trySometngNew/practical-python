7c09750e275e7d67bb54c318de7a37a8d06d140d
Sourabh Lonikar <sourabhlonikar@gmail.com>
2020-06-06 18:10:11 +0530

    Exercise 1.28: Other kinds of “files” ::  Solution

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,18 @@
 # pcost.py
 # 
-# Exercise 1.27
+# Exercise 1.28
+
+import gzip
+
 total_shares_cost = 0.0
 
-with open('Data/portfolio.csv', 'rt') as f:
-    header = next(f);
+with gzip.open('Data/portfolio.csv.gz') as f:
     for line in f:
-        share_entry_list = line.split(',')
-        share_cost = int(share_entry_list[1]) * float(share_entry_list[2])
-        total_shares_cost = total_shares_cost + share_cost
+        print(line)
+#     header = next(f);
+#     for line in f:
+#         share_entry_list = line.split(',')
+#         share_cost = int(share_entry_list[1]) * float(share_entry_list[2])
+#         total_shares_cost = total_shares_cost + share_cost
 
-print(f'Total Portfolio Value is ${total_shares_cost}')
+# print(f'Total Portfolio Value is ${total_shares_cost}')
